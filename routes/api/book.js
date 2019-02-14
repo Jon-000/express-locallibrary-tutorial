82084a3c32860ef1895b3b8191dc60d1ec69579b
Jon <stftrack@gmail.com>
2019-02-14 08:43:10 +0800

    add cors to api route

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+
+const Book = require('../../models/book');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("book api")
+  Book
+    .find()
+    .populate('author')
+    .exec((err, result) => {
+      if (err) next (err)
+      res.status(200).json({msg:"find all books successfully", book_list: result});
+    })
 });
+
+router.delete('/:id', (req, res, next) => {
+  console.log(req.params.id)
+  res.send(req.params.id)
+})
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors')
 
 const bookApi = require('./book');
 
-/* GET home page. */
+router.use(cors())
+
 router.get('/', function(req, res, next) {
   res.send("api root")
 });
